7b7d6e998ee999944754f6f1134d70541dce1a2d
SumedhJ2797 <sumedhj27@gmail.com>
2025-06-08 17:55:43 +0530

    updated

--- src/main/java/com/yhayashi30/sample/App.java
+++ src/main/java/com/yhayashi30/sample/App.java
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory;
  */
 public class App 
 {
-    private static Logger logger = LoggerFactory.getLogger(App.class);
+    private static Logger logger = LoggerFactory.getLogger(App.clas;
     public static void main( String[] args )
     {
         logger.info("Hello World!");
